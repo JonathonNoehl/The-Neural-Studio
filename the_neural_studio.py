6e14fb36b2d6d252d6e433102b6217dc61220d16
Jonathon Noehl <800429786@umpqua.edu>
2026-09-17 12:52:55 -0700

    Add The Neural Studio application name and title metadata

---
+++ the_neural_studio.py
@@ -0,0 +1,9 @@
+"""Branding metadata for The Neural Studio.
+
+Import these constants wherever the application displays its name or title.
+"""
+
+APP_NAME = "The Neural Studio"
+APP_TITLE = "The Neural Studio: Medical-Grade Auditory Neuromodulation"
+
+__all__ = ["APP_NAME", "APP_TITLE"]
